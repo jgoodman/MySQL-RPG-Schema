08fc8bacaf05dc16ac309c31ef073a480700ad73
jgoodman <jgoodman1990@gmail.com>
2014-05-10 21:39:41 -0600

    Update create_tables.sql
    Added "alive", "level", "xp", and "money" columns to character table. Added durability to item table. Added "NOT NULL" and "UNSIGNED" constraints to many columns. Updated "value" from varchar to bigint in character_attribute and item_attribute tables. Updated "x" and "y" columns from varchar to bigint in location table.

--- create_tables.sql
+++ create_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `conf` (
-    `conf_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `conf_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     `value` varchar(255),
     PRIMARY KEY (`conf_id`),
     CONSTRAINT `uk_name` UNIQUE (`name`)
 );
 
 CREATE TABLE `user_type` (
-    `user_type_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `user_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`user_type_id`)
 );
 
@@ -18,19 +18,19 @@ INSERT INTO `user_type` (`name`) VALUES ('player');
 INSERT INTO `user_type` (`name`) VALUES ('spectator');
 
 CREATE TABLE `user` (
-    `user_id` int(11) NOT NULL AUTO_INCREMENT,
-    `user_type_id` int(11),
-    `first_name` varchar(255),
-    `last_name` varchar(255),
-    `email` varchar(255),
-    `password` varchar(255),
+    `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_type_id` int(11) UNSIGNED NOT NULL,
+    `first_name` varchar(255) NOT NULL,
+    `last_name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
     PRIMARY KEY (`user_id`),
     CONSTRAINT `fk_user_user_type_id` FOREIGN KEY (`user_type_id`) REFERENCES `user_type`(`user_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `character_type` (
-    `character_type_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `character_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`character_type_id`)
 );
 
@@ -38,17 +38,21 @@ INSERT INTO `character_type` (`name`) VALUES ('player');
 INSERT INTO `character_type` (`name`) VALUES ('non-playable');
 
 CREATE TABLE `character` (
-    `character_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_type_id` int(11),
-    `name` varchar(255),
+    `character_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_type_id` int(11) UNSIGNED NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `alive` tinyint NOT NULL,
+    `level` smallint NOT NULL,
+    `xp` bigint NOT NULL,
+    `money` bigint NOT NULL,
     PRIMARY KEY (`character_id`),
     CONSTRAINT `fk_character_character_type_id` FOREIGN KEY (`character_type_id`) REFERENCES `character_type`(`character_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `user_character` (
-    `user_character_id` int(11) NOT NULL AUTO_INCREMENT,
-    `user_id` int(11),
-    `character_id` int(11),
+    `user_character_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) UNSIGNED NOT NULL,
+    `character_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`user_character_id`),
     CONSTRAINT `uk_user_id_character_id` UNIQUE (`user_id`, `character_id`),
     CONSTRAINT `fk_user_character_user_id` FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -56,8 +60,8 @@ CREATE TABLE `user_character` (
 );
 
 CREATE TABLE `attribute` (
-    `attribute_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `attribute_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     `desc` varchar(255),
     PRIMARY KEY (`attribute_id`)
 );
@@ -74,10 +78,10 @@ INSERT INTO `attribute` (`name`, `desc`) VALUES ('luck', 'A measure of a charact
 INSERT INTO `attribute` (`name`, `desc`) VALUES ('damage', 'A measure to inflict damage');
 
 CREATE TABLE `character_attribute` (
-    `character_attribute_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_id` int(11),
-    `attribute_id` int(11),
-    `value` varchar(255),
+    `character_attribute_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) UNSIGNED NOT NULL,
+    `attribute_id` int(11) UNSIGNED NOT NULL,
+    `value` bigint NOT NULL,
     PRIMARY KEY (`character_attribute_id`),
     CONSTRAINT `uk_character_id_attribute_id` UNIQUE (`character_id`, `attribute_id`),
     CONSTRAINT `fk_character_attribute_character_id` FOREIGN KEY (`character_id`) REFERENCES `character`(`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -85,25 +89,25 @@ CREATE TABLE `character_attribute` (
 );
 
 CREATE TABLE `place` (
-    `place_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `place_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`place_id`)
 );
 
 CREATE TABLE `location` (
-    `location_id` int(11) NOT NULL AUTO_INCREMENT,
-    `x` varchar(255),
-    `y` varchar(255),
-    `place_id` int(11),
+    `location_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `x` bigint NOT NULL,
+    `y` bigint NOT NULL,
+    `place_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`location_id`),
     CONSTRAINT `uk_x_y` UNIQUE (`x`, `y`),
     CONSTRAINT `fk_location_place_id` FOREIGN KEY (`place_id`) REFERENCES `place`(`place_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `character_location` (
-    `character_location_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_id` int(11),
-    `location_id` int(11),
+    `character_location_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) UNSIGNED NOT NULL,
+    `location_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`character_location_id`),
     CONSTRAINT `uk_character_id` UNIQUE (`character_id`),
     CONSTRAINT `fk_character_location_character_id` FOREIGN KEY (`character_id`) REFERENCES `character`(`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -111,25 +115,26 @@ CREATE TABLE `character_location` (
 );
 
 CREATE TABLE `item_type` (
-    `item_type_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `item_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     `desc` varchar(255),
     PRIMARY KEY (`item_type_id`)
 );
 
 CREATE TABLE `item` (
-    `item_id` int(11) NOT NULL AUTO_INCREMENT,
-    `item_type_id` int(11),
-    `name` varchar(255),
+    `item_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `item_type_id` int(11) UNSIGNED NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `durability` tinyint  NOT NULL,
     PRIMARY KEY (`item_id`),
     CONSTRAINT `fk_item_item_type_id` FOREIGN KEY (`item_type_id`) REFERENCES `item_type`(`item_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `item_attribute` (
-    `item_attribute_id` int(11) NOT NULL AUTO_INCREMENT,
-    `item_id` int(11),
-    `attribute_id` int(11),
-    `value` varchar(255),
+    `item_attribute_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `item_id` int(11) UNSIGNED NOT NULL,
+    `attribute_id` int(11) UNSIGNED NOT NULL,
+    `value` bigint NOT NULL,
     PRIMARY KEY (`item_attribute_id`),
     CONSTRAINT `uk_item_id_attribute_id` UNIQUE (`item_id`, `attribute_id`),
     CONSTRAINT `fk_item_attribute_item_id` FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -137,9 +142,9 @@ CREATE TABLE `item_attribute` (
 );
 
 CREATE TABLE `item_location` (
-    `item_location_id` int(11) NOT NULL AUTO_INCREMENT,
-    `item_id` int(11),
-    `location_id` int(11),
+    `item_location_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `item_id` int(11) UNSIGNED NOT NULL,
+    `location_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`item_location_id`),
     CONSTRAINT `uk_item_id` UNIQUE (`item_id`),
     CONSTRAINT `fk_item_location_item_id` FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -147,9 +152,9 @@ CREATE TABLE `item_location` (
 );
 
 CREATE TABLE `character_item` (
-    `character_item_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_id` int(11),
-    `item_id` int(11),
+    `character_item_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) UNSIGNED NOT NULL,
+    `item_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`character_item_id`),
     CONSTRAINT `uk_character_id_item_id` UNIQUE (`character_id`, `item_id`),
     CONSTRAINT `fk_character_item_character_id` FOREIGN KEY (`character_id`) REFERENCES `character`(`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -157,8 +162,8 @@ CREATE TABLE `character_item` (
 );
 
 CREATE TABLE `equipment_slot` (
-    `equipment_slot_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `equipment_slot_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`equipment_slot_id`),
     CONSTRAINT `uk_name` UNIQUE (`name`)
 );
@@ -179,10 +184,10 @@ INSERT INTO `equipment_slot` (`name`) VALUES ('main_hand');
 INSERT INTO `equipment_slot` (`name`) VALUES ('off_hand');
 
 CREATE TABLE `character_equipment` (
-    `character_equipment_id` int(11),
-    `equipment_slot_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_id` int(11),
-    `item_id` int(11),
+    `character_equipment_id` int(11) UNSIGNED NOT NULL,
+    `equipment_slot_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) UNSIGNED NOT NULL,
+    `item_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`equipment_slot_id`),
     CONSTRAINT `uk_equipment_slot_id_character_id` UNIQUE (`equipment_slot_id`, `character_id`),
     CONSTRAINT `fk_character_equipment_equipment_slot_id` FOREIGN KEY (`equipment_slot_id`) REFERENCES `equipment_slot`(`equipment_slot_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -191,16 +196,16 @@ CREATE TABLE `character_equipment` (
 );
 
 CREATE TABLE `class` (
-    `class_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `class_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`class_id`),
     CONSTRAINT `uk_name` UNIQUE (`name`)
 );
 
 CREATE TABLE `character_class` (
-    `character_class_id` int(11) NOT NULL AUTO_INCREMENT,
-    `character_id` int(11),
-    `class_id` int(11),
+    `character_class_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) UNSIGNED NOT NULL,
+    `class_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`character_class_id`),
     CONSTRAINT `uk_character_id` UNIQUE (`character_id`),
     CONSTRAINT `fk_character_class_character_id` FOREIGN KEY (`character_id`) REFERENCES `character`(`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -208,24 +213,24 @@ CREATE TABLE `character_class` (
 );
 
 CREATE TABLE `ability_type` (
-    `ability_type_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
+    `ability_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
     `desc` varchar(255),
     PRIMARY KEY (`ability_type_id`)
 );
 
 CREATE TABLE `ability` (
-    `ability_id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255),
-    `ability_type_id` int(11),
+    `ability_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `ability_type_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`ability_id`),
     CONSTRAINT `fk_ability_ability_type_id` FOREIGN KEY (`ability_type_id`) REFERENCES `ability`(`ability_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `class_ability` (
-    `class_ability_id` int(11) NOT NULL AUTO_INCREMENT,
-    `class_id` int(11),
-    `ability_id` int(11),
+    `class_ability_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `class_id` int(11) UNSIGNED NOT NULL,
+    `ability_id` int(11) UNSIGNED NOT NULL,
     PRIMARY KEY (`class_ability_id`),
     CONSTRAINT `uk_class_id_ability_id` UNIQUE (`class_id`, `ability_id`),
     CONSTRAINT `fk_class_ability_class_id` FOREIGN KEY (`class_id`) REFERENCES `class`(`class_id`) ON DELETE CASCADE ON UPDATE CASCADE,
